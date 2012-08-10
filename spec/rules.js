9d903b01a4a829cb27379d55bee37a4605f66c49
Konrad Paumann <konrad.paumann@gmail.com>
2012-08-10 14:21:43 +0200

    mit rene

--- spec/rules.js
+++ spec/rules.js
@@ -1,70 +1,44 @@
-describe("Rules", function() {
+
+describe("Cell", function() {
+  it("should return 0 as the count of living neighbours", function() {
+    var cell = new Cell(1,1);
+    expect(cell.calculateLivingNeighbours()).toEqual(0);
+  });
   
-  it("should should die if it has less than two neighbors", function() {
-    expect(lonely(true)).toBeFalsy();
-  });
-  it("should should stay alive if it has two or three neighbors", function() {
-    expect(normal(true)).toBeTruthy();
-    expect(optimal(true)).toBeTruthy();
-  });
-
-  it("should should die if it has more than thre neighbors", function() {
-    expect(crowded(true)).toBeFalsy();
-  });
-  it("should should come to live if it has exactly three neighbors", function() {
-    expect(optimal(false)).toBeTruthy();
-  });
-});
-
-describe("Neighbourhoods",function(){
-
-  it("should return a lonely neighbourhoood for 1 neighbours", function(){
-    expect(neighbourhood(1)).toEqual(lonely);
-  });
-
-  it("should return a normal neighbourhoood for 2 neighbours", function(){
-    expect(neighbourhood(2)).toEqual(normal);
-  });
-
-  it("should return a optimal neighbourhoood for 3 neighbours", function(){
-    expect(neighbourhood(3)).toEqual(optimal);
-  });
-
-  it("should return a crowded neighbourhoood for 4 neighbours", function(){
-    expect(neighbourhood(4)).toEqual(crowded);
-  });
-
-});
-describe("Cell",function(){
-  it("should calculate living should return zero by default", function(){
-    expect(new Cell().calculateAliveNeighbours()).toEqual(0);
-  });
-
-
-  it("should calculate living should return 1 with one living neighbour", function(){
-    var cell = new Cell();
-    var other = new Cell();
-    other.cellstate = Cellstate.alive;
+  it("should return 1 as the count of living neighbours", function() {
+    var cell = new Cell(1,1);
+    var other = new Cell(1,2);
+    other.state = CellState.alive;
     cell.addNeighbour(other);
-    expect(cell.calculateAliveNeighbours()).toEqual(1);
+    expect(cell.calculateLivingNeighbours()).toEqual(1);
   });
-
-  it("should calculate living should return 0 with only dead neighbours", function(){
-    var cell = new Cell();
-    cell.addNeighbour(new Cell);
-    expect(cell.calculateAliveNeighbours()).toEqual(0);
-  });
-
 });
 
-describe("Board",function(){
-
-
-  it("should allow to add new Cells ", function(){
-    var board = new Board();
-    board.addLivingCell(3, 5);
-    var cell = board.getCell(3,5);
-    expect(cell.calculateAliveNeighbours()).toEqual(0);
+describe("Board", function() {
+  it("should return 0 as the count of living neighbours", function() {
+    var cell = new Cell(1,1);
+    expect(cell.calculateLivingNeighbours()).toEqual(0);
   });
+});
 
+describe("Rules", function() {
+  it("should die if less than 2 alive neighbours", function() {
+    var cell = new Cell(1,1);
+    cell.state = CellState.alive;
+    var aliveCell = new Cell(1,2);
+    aliveCell.state = CellState.alive;
+    cell.addNeighbour(aliveCell);
+    expect(calculateState(cell)).toEqual(CellState.dead);
+  });
+  it("should live if 2 or three alive neighbours", function() {
+    var cell = new Cell(1,1);
+    cell.state = CellState.alive;
+    var aliveCell = new Cell(1,2);
+    aliveCell.state = CellState.alive;  
+    var aliveCell2 = new Cell(1,3);
+    aliveCell2.state = CellState.alive;
+    cell.addNeighbour(aliveCell);
+    cell.addNeighbour(aliveCell2);
+    expect(calculateState(cell)).toEqual(CellState.alive);
+  });
 });
--- src/rules.js
+++ src/rules.js
@@ -1,48 +1,89 @@
-var lonely = function(alive){
-    return Cellstate.dead;
-}
-var normal = function(alive) {
-    return alive;
-}
-var optimal =function(alive) {
-    return Cellstate.alive;
-}
-var crowded = function(alive) {
-    return Cellstate.dead;
-}
 
-function neighbourhood(neighbours) {
-    if(neighbours==2){
-        return normal;
-    }
-    if(neighbours==3) {
-        return optimal;
-    }
-    if(neighbours > 3){
-        return crowded;
-    }
+CellState = {"dead":0, "alive":1};
 
-    return lonely;
-}
-
-
-var Cellstate = { "dead":0, "alive":1};
-
-var Cell=function() {
-    this.cellstate = Cellstate.dead;
+Cell = function(x, y) {
+    this.state = CellState.dead;
     this.neighbours = [];
+    this.x = x;
+    this.y = y;
+}
+
+Cell.prototype.calculateLivingNeighbours = function() {
+    var livingNeighboursCount = 0;
+    var ns = this.neighbours.length;
+    console.log(ns);
+    for (var i = 0; i < ns; i++) {
+        if (this.neighbours[i].state === CellState.alive) {
+            livingNeighboursCount++;
+        }
+    }
+    return livingNeighboursCount;
 }
 
 Cell.prototype.addNeighbour = function(neighbour) {
     this.neighbours.push(neighbour);
 }
 
-Cell.prototype.calculateAliveNeighbours = function() {
-    var count = 0;
-    for (var i = 0; i < this.neighbours.length; i++) {
-        if (this.neighbours[i].cellstate === Cellstate.alive) {
-            count++;
+Board = function() {
+    this.cells = [];
+    this.cells.push([]);
+}
+
+Board.prototype.init = function(xSize, ySize) {
+    var neighbour;
+    for(x = 0; x < xSize; x++) {
+        for(y=0; y < ySize; y++) {
+            var cell = new Cell(x,y);
+            this.cells[x][y] = cell;
         }
     }
-    return count;
+
+    for(x = 0; x < xSize; x++) {
+        for(y=0; y < ySize; y++) {
+            var cell = this.cells[x][y];
+            if(x > 0) {
+                neighbour = this.cells[x-1][y];
+                cell.neighbours.push(neighbour);
+            }
+            if(x < xSize) {
+                neighbour = this.cells[x+1][y];
+                cell.neighbours.push(neighbour);
+            }
+            if(y > 0) {
+                neighbour = this.cells[x][y-1];
+                cell.neighbours.push(neighbour);
+            }
+            if(y < ySize) {
+                neighbour = this.cells[x][y+1];
+                cell.neighbours.push(neighbour);
+            }
+            if(x > 0 && y > 0) {
+                neighbour = this.cells[x-1][y-1];
+                cell.neighbours.push(neighbour);
+            }
+            if(x < xSize && y < ySize) {
+                neighbour = this.cells[x+1][y+1];
+                cell.neighbours.push(neighbour);
+            }
+            if(x < xSize && y > 0){
+                neighbour = this.cells[x+1][y-1];
+                cell.neighbours.push(neighbour);
+            }
+            if(y < ySize && x > 0) {
+                neighbour = this.cells[x-1][y+1];
+                cell.neighbours.push(neighbour);   
+            }
+        }
+    }
+}
+
+function calculateState(cell) {
+    var livingNeighbours = cell.calculateLivingNeighbours();
+    if(cell.state === CellState.alive && livingNeighbours < 2) {
+        return CellState.dead;
+    }
+    if(cell.state === CellState.alive && livingNeighbours >=2 && livingNeighbours <=3) {
+        return CellState.alive;
+    }
+    return CellState.dead;
 }
